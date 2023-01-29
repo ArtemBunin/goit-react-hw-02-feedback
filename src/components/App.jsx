9676ac6febe9cc7ab232c7c7a96c919d0acd8d29
ArtemBunin <artembunin93@gmail.com>
2023-01-29 22:50:00 +0000

    add commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+import { Statistics } from './Statistics/Statistics';
+import { Box } from './App.styled';
+ export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onHandlClick = propName => {
+  
+    this.setState(prevState =>
+      ({[propName.target.name]: prevState[propName.target.name] + 1
+      
+    }));
+  };
+  countTotal = () => this.state.good + this.state.bad + this.state.neutral;
+
+  countPositivFeedbeack = () =>
+    ((this.state.good / this.countTotal()) * 100).toFixed(0);
+  render() {
+    const { good, neutral, bad } = this.state;
+    const options = Object.keys(this.state);
+    return (
+      <Box>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onHandlClick}
+          />
+        </Section>
+
+        <Section title={'Statistics'}>
+          {this.countTotal() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotal()}
+              positivePercentage={this.countPositivFeedbeack()}
+            />
+          )}
+        
+        </Section>
+      </Box>
+    );
+  }
+}
+
---
+++ src/components/App.styled.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+export const Box = styled.div`
+height: 100vh;
+display: flex;
+flex-wrap: wrap;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+font-size: 40;
+color: #010101;
+background-color:grey;
+width:400px
+`;
